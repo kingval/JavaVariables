8c06cc607ee25b6f4729805bd71607658f2e3d5e
kingval <king.valand@gmail.com>
2023-01-10 17:51:39 +0000

    renamed a file in java homework week 2 update 2

--- Constant2.java
+++ Constant2.java
@@ -1,6 +1,6 @@
 package A.Variables;
 
-public class Constant {  //something that will not be changed during the course or running of the programme (Variable is the opposite of this)
+public class Constant2 {  //something that will not be changed during the course or running of the programme (Variable is the opposite of this)
 
     static final String BANK_NAME= "Barclays";
 
